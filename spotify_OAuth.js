77eda98aa05a234d5cc3b8dbce80e14eb9f78362
AzizhanKaya <kayaazizhan3@gmail.com>
2024-10-28 23:19:21 +0300

    Redicret URL corrected

--- spotify_OAuth.js
+++ spotify_OAuth.js
@@ -1,5 +1,5 @@
 const clientId = 'a93446e4b03243dc9277d86767c38e72';
-export const redirectUri = 'http://localhost:8000';
+export const redirectUri = 'https://azizhankaya.github.io/spotify-russian-playlist';
 const scopes = 'user-read-private user-read-email playlist-read-private user-library-read playlist-modify-private';
 
 const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
